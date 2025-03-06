6d12cb69ead8ef6de98e15396feaf8e8e150e897
Dannae <danavalegg15@gmail.com>
2025-03-06 00:41:46 -0500

    se agrega dos archivos sql para la base de datos de las tablas cliente y ticket_soporte

---
+++ Database/cliente.sql
@@ -0,0 +1,12 @@
+INSERT INTO cliente (tipo_documento_id, nombre, direccion, telefono, celular, correo, ciudad_id, fecha_registro, estado_contrato_id, notificacion_id
+) VALUES
+(1, 'Juan Pérez', 'Calle 123, Ciudad A', '123-456-789', '987-654-321', 'juan@example.com', 1, '2023-01-01', 1, 2),
+(2, 'María Gómez', 'Avenida 456, Ciudad B', '234-567-890', '876-543-210', 'maria@example.com', 2, '2023-02-15', 2, 1),
+(1, 'Carlos López', 'Carrera 789, Ciudad C', '345-678-901', '765-432-109', 'carlos@example.com', 3, '2023-03-10', 1, 3),
+(1, 'Ana Martínez', 'Calle 321, Ciudad D', '456-789-012', '654-321-098', 'ana@example.com', 4, '2023-04-20', 2, 1),
+(2, 'Luis Rodríguez', 'Avenida 654, Ciudad E', '567-890-123', '543-210-987', 'luis@example.com', 5, '2023-05-05', 1, 1),
+(3, 'Sofía García', 'Carrera 987, Ciudad F', '678-901-234', '432-109-876', 'sofia@example.com', 6, '2023-06-15', 2, 3),
+(1, 'Pedro Sánchez', 'Calle 654, Ciudad G', '789-012-345', '321-098-765', 'pedro@example.com', 7, '2023-07-01', 1, 3),
+(2, 'Laura Fernández', 'Avenida 321, Ciudad H', '890-123-456', '210-987-654', 'laura@example.com', 8, '2023-08-10', 2, 2),
+(3, 'Diego Torres', 'Carrera 123, Ciudad I', '901-234-567', '109-876-543', 'diego@example.com', 9, '2023-09-20', 1, 1),
+(1, 'Carmen Ruiz', 'Calle 987, Ciudad J', '012-345-678', '098-765-432', 'carmen@example.com', 10, '2023-10-05', 2, 1);
---
+++ Database/ticket_soporte.sql
@@ -0,0 +1,13 @@
+INSERT INTO ticket_soporte (
+    cliente_servicio_id, usuario_externo_id, estado_soporte_id, ANS_id
+) VALUES
+(1, 1, 1, 1),
+(2, 2, 2, 2),
+(3, 3, 3, 3),
+(4, 4, 1, 4),
+(5, 5, 2, 5),
+(6, 6, 3, 6),
+(7, 7, 1, 7),
+(8, 8, 2, 8),
+(9, 9, 3, 9),
+(10, 10, 1, 10);
